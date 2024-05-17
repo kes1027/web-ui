c6ad73e9f6f305aeff08a54b905b5239b94cec3a
김은소 <k.eunso1027@gmail.com>
2024-05-17 10:05:17 +0900

    기본 구조 및 디자인 완성

--- 01-GNB/03/js/script.js
+++ 01-GNB/03/js/script.js
@@ -1,7 +1,5 @@
 $(function () {
   // 대상을 변수에 저장
-  const $window =
-    $(window); /* '' 안에 쓰지 않고, jquery 매소드를 사용하려면 제이커리의 객체가 되어야 하기 때문에 $ 사용 */
   const $header = $('header');
   const $menu = $('.gnb > li');
   const $submenu = $('.submenu');
@@ -22,21 +20,6 @@ $(function () {
   $menu.on('mouseleave', function () {
     $submenu.stop().slideUp(duration);
     $menu.removeClass('on');
-
     $header.removeClass('active');
-  });
-
-  // 마우스 휠을 조작할 때
-  $window.on('wheel', function (e) {
-    // console.log(e);
-    // wheel delta : 음수면 휠 아래로 내렸다는 뜻, 양수면 휠 위로 올렸다는 뜻
-
-    if (e.originalEvent.wheelDelta > 0) {
-      // 휠을 올렸을 때
-      $header.removeClass('hide');
-    } else {
-      // 휠을 내렸을 때
-      $header.addClass('hide');
-    }
   });
 });
